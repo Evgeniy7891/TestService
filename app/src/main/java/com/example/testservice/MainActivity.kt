bf01416d87dfc96147216d211a748fd164aac857
Evgeniy Sterekhov <john2331832@gmail.com>
2023-11-23 17:19:39 +0500

    Add JobScheduler

--- app/src/main/java/com/example/testservice/MainActivity.kt
+++ app/src/main/java/com/example/testservice/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.testservice
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -32,6 +35,17 @@ class MainActivity : AppCompatActivity() {
 
         binding.intentService.setOnClickListener {
             ContextCompat.startForegroundService(this, MyIntentService.newIntent(this))
+        }
+        binding.jobDispatcher.setOnClickListener {
+            val componentName = ComponentName( this, MyJobService::class.java)
+            val jobInfo = JobInfo.Builder(MyJobService.JOB_ID, componentName)
+                .setRequiresCharging(true)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .setPersisted(true)
+                .build()
+
+            val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
+            jobScheduler.schedule(jobInfo)
         }
     }
 }
